b262d30e1b3ca8b9ca513ed47c3174a77ea4ee45
Nathan Thomas <nwthomas@me.com>
2021-09-11 14:51:21 -0700

    Add ability to wave and display new totalWaves

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import * as React from "react";
+import { ethers } from "ethers";
 import styled from "styled-components";
 import GlobalStyles from "./styles";
+import abi from "./utils/wave";
 
 const checkIfWalletIsConnected = () => {
   const { ethereum } = window;
@@ -27,9 +29,12 @@ const checkIfWalletIsConnected = () => {
 
 function App() {
   const [account, setAccount] = React.useState("");
+  const [totalWaves, setTotalWaves] = React.useState(null);
+  const contractAbi = abi.abi;
+
   React.useEffect(() => {
     checkIfWalletIsConnected();
-  }, []);
+  }, [account]);
 
   const handleConnectWalletClick = () => {
     const { ethereum } = window;
@@ -39,7 +44,7 @@ function App() {
     }
 
     ethereum.request({ method: "eth_requestAccounts" }).then((accounts) => {
-      if (accounts.length !== 0) {
+      if (accounts.length >= 0) {
         console.log("Connected", accounts[0]);
         setAccount(account);
       } else {
@@ -48,17 +53,38 @@ function App() {
     });
   };
 
+  const handleWave = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const waveContract = new ethers.Contract(
+      process.env.REACT_APP_CONTRACT_ADDRESS,
+      contractAbi,
+      signer
+    );
+
+    const waveTxn = await waveContract.wave();
+    console.log("Mining...", waveTxn.hash);
+
+    await waveTxn.wait();
+    console.log("Mined -- ", waveTxn.hash);
+
+    let count = await waveContract.getTotalWaves();
+    setTotalWaves(count);
+    console.log("Retrieved total wave count", count.toNumber());
+  };
+
   return (
     <>
       <GlobalStyles />
       <RootStyles>
         <h1>Hey, I'm Nathan! 👋🏻</h1>
-        <button>Wave at me</button>
+        <button onClick={handleWave}>Wave at me</button>
         <div>
           <ConnectWalletButton onClick={handleConnectWalletClick}>
             {account.length ? "Connect Wallet" : "Connected!"}
           </ConnectWalletButton>
         </div>
+        {totalWaves ? <h3>{`Total waves: ${totalWaves}`}</h3> : null}
       </RootStyles>
     </>
   );
@@ -72,6 +98,10 @@ const RootStyles = styled.div`
   padding: 50px 5%;
   position: relative;
   width: 100%;
+
+  > h3 {
+    margin-top: 30px;
+  }
 `;
 
 const ConnectWalletButton = styled.button`
