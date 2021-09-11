b74b2a0407f49af82103bf0d9f62ef5da745fa84
Nathan Thomas <nwthomas@me.com>
2021-09-11 11:52:19 -0700

    Update run script

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,17 @@
 async function main() {
+  const [owner, randoPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
-  console.log("The Contract deployed to:", waveContract.address);
+  console.log("The contract deployed to:", waveContract.address);
+  console.log("The contract deployed by:", owner.address);
+
+  let waveCount = await waveContract.getTotalWaves();
+
+  const waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
 }
 
 main()
