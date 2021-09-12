ecdc67c6843a8fd8447806bc5c5864f436d0e4af
Nathan Thomas <nwthomas@me.com>
2021-09-11 17:17:49 -0700

    Update contract to be payable on front-end

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,10 @@
 async function main() {
-  const [deployer] = await hre.ethers.getSigners();
-
-  console.log("Deploying contracts with the account:", deployer.address);
-
-  console.log("Account balance:", (await deployer.getBalance()).toString());
-
-  const Token = await hre.ethers.getContractFactory("WavePortal");
-  const token = await Token.deploy();
-
-  console.log("WavePortal address:", token.address);
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy({
+    value: ethers.utils.parseEther("0.1"),
+  });
+  await waveContract.deployed();
+  console.log("WavePortal address:", waveContract.address);
 }
 
 main()
